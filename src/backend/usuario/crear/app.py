496c1f6d6456a102fe734367a8dbee537b49d320
carlos <carloscariasingenieria@gmail.com>
2021-06-04 01:06:25 -0600

    obtener datos de post crear usuario

--- src/backend/usuario/crear/app.py
+++ src/backend/usuario/crear/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request
 import os
 import pymongo
 
@@ -13,7 +13,43 @@ client = pymongo.MongoClient(db_host, db_port)
 db = client[str(db_name)]
 col = db["usuarios"]
 
+class Usuario:
+    def __init__(self, tipo,nombre,apellido,correo,password,telefono,direccion):
+        self.tipo = tipo
+        self.nombre = nombre
+        self.apellido = apellido
+        self.correo = correo
+        self.password = password
+        self.telefono = telefono
+        self.direccion = direccion
+    
+    def toJson(self):
+        return {
+            "tipo":self.tipo,
+            "nombre":self.nombre,
+            "apellido":self.apellido,
+            "correo": self.correo,
+            "password":self.password,
+            "telefono":self.telefono,
+            "direccion":self.direccion
+        }
+
+@app.route("/create", methods=["POST"])
+def login():
+    tipo = request.form.get("tipo","cliente")
+    nombre = request.form["nombre"]
+    apellido = request.form["apellido"]
+    correo = request.form["correo"]
+    password = request.form["password"]
+    telefono = request.form["telefono"]
+    direccion = request.form["direccion"]
+    new_user = Usuario(tipo,nombre,apellido,correo,password,telefono,direccion)
+    print(new_user.toJson())
+    return "Ok"
 
 @app.route("/")
 def main():
     return "<p>usuario_crear</p>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",debug=True)
