9b5ff918ab782146164fd6441cb2582f51c2c42f
Guillermo Peitzner <guillermopeitzner@gmail.com>
2021-06-11 11:54:21 -0600

    [add] dev mode script

--- src/backend/orden/editar/app.py
+++ src/backend/orden/editar/app.py
@@ -8,8 +8,10 @@ db_host = os.environ["db_host"] if "db_host" in os.environ else "localhost"
 db_password = os.environ["db_password"] if "db_password" in os.environ else ""
 db_port = int(os.environ["db_port"]) if "db_port" in os.environ else 27017
 db_name = os.environ["db_name"] if "db_name" in os.environ else "ezread"
+db_user = os.environ["db_user"] if "db_user" in os.environ else ""
 
-client = pymongo.MongoClient(db_host, db_port)
+client = pymongo.MongoClient(
+    host=db_host, port=db_port, username=db_user, password=db_password)
 db = client[str(db_name)]
 col = db["ordenes"]
 
