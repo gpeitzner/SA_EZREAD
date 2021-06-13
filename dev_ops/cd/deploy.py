7fff82868fc1a898f4c1c8129f000aa2b0e4e4a2
Guillermo Peitzner <guillermopeitzner@gmail.com>
2021-06-13 01:10:03 -0600

    [test] path

--- dev_ops/cd/deploy.py
+++ dev_ops/cd/deploy.py
@@ -8,11 +8,29 @@ Connection(cd_host).run("sudo apt update")
 Connection(cd_host).run("sudo apt upgrade")
 Connection(cd_host).run("sudo apt autoremove")
 Connection(cd_host).run("sudo snap install docker")
-Connection(cd_host).run("rm ./docker-compose-prod.yml")
-Connection(cd_host).run("rm ./.env-prod")
-Connection(cd_host).put("./docker-compose-prod.yml",
-                        "./docker-compose-prod.yml")
-Connection(cd_host).put("./.env-prod", "./.env-prod")
+
+
+try:
+    Connection(cd_host).run("rm ./docker-compose-prod.yml")
+except:
+    print("docker-compose-prod.yml doesn't exists")
+try:
+    Connection(cd_host).run("rm ./.env-prod")
+except:
+    print(".env-prod doesn't exist")
+
+
+try:
+    Connection(cd_host).put("./docker-compose-prod.yml",
+                            "./docker-compose-prod.yml")
+except:
+    print("docker-compose-prod.yml already exists")
+try:
+    Connection(cd_host).put("./.env-prod", "./.env-prod")
+except:
+    print(".env-prod already exists")
+
+
 Connection(cd_host).run(
     "sudo docker-compose -f ./docker-compose-prod.yml pull")
 Connection(cd_host).run(
