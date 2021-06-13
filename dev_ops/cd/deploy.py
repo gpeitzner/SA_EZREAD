8dc312f0b18307f9153b8910aa57bd069ed48b98
Guillermo Peitzner <guillermopeitzner@gmail.com>
2021-06-13 01:05:22 -0600

    [test] path

--- dev_ops/cd/deploy.py
+++ dev_ops/cd/deploy.py
@@ -9,7 +9,7 @@ Connection(cd_host).run("sudo apt upgrade")
 Connection(cd_host).run("sudo apt autoremove")
 Connection(cd_host).run("sudo snap install docker")
 Connection(cd_host).run("rm ./docker-compose-prod.yml")
-Connection(cd_host).run("rm ./env-prod-prod.yml")
+Connection(cd_host).run("rm ./.env-prod")
 Connection(cd_host).put("./docker-compose-prod.yml",
                         "./docker-compose-prod.yml")
 Connection(cd_host).put("./.env-prod", "./.env-prod")
