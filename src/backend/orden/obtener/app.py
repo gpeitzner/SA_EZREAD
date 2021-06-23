d2bf1af09ed4e981cb8219fd954f3d208bc617db
carlos <carloscariasingenieria@gmail.com>
2021-06-23 08:58:27 -0600

    Metodo para enviar pdf a usuario

--- src/backend/orden/obtener/app.py
+++ src/backend/orden/obtener/app.py
@@ -3,6 +3,8 @@ from bson.objectid import ObjectId
 import os
 import pymongo
 from flask_cors import CORS
+import smtplib
+
 #OBTENER
 app = Flask(__name__)
 CORS(app)
@@ -27,8 +29,40 @@ def create():
         return {"usuario":existe['usuario'],"estado":existe['estado'],"libros":existe['libros']}
     else: #no existe
         return {"mensaje":"No tiene ordenes"}
+
+@app.route("/factura", methods=["GET"])
+def getFactura():
+    #CODIGO PARA GENERAR factura en pdf
+    enviarFactura(mensaje="Mensaje a enviar",correo_destino="correo del usuario")
+
+def enviarFactura(mensaje,correo_destino):
+    #import email
+    from email.mime.application import MIMEApplication
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    
+    server.login('ezreadcorp@gmail.com', 'ezread123')
+    msg = MIMEMultipart()
+
+    message = mensaje
+    msg['Subject'] = "Factura de Compra EZREAD"
+    msg['From'] = 'ezreadcorp@gmail.com'
+    msg['To'] = correo_destino
+    msg.attach(MIMEText(message, "plain"))
+    with open("./factura.pdf", "rb") as f:
+        attach = MIMEApplication(f.read(),_subtype="pdf")
+    attach.add_header('Content-Disposition','attachment',filename=str("FacturaElectronica.pdf"))
+    msg.attach(attach)
+    server.send_message(msg)
+
 @app.route("/")
 def main():
     return "<p>orden_obtener</p>"
+
+
+
 #if __name__ == "__main__":
     #app.run(host="0.0.0.0",debug=True,port=5013)
