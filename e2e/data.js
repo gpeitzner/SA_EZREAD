23aa5c8e0a1193a144f3fc8cfd124a49be9ede98
Guillermo Peitzner <guillermopeitzner@gmail.com>
2021-06-24 01:00:47 -0600

    [add] good login test

--- e2e/data.js
+++ e2e/data.js
@@ -3,7 +3,7 @@ db.usuarios.insertOne({
 	nombre: "Guillermo",
 	apellido: "Peitzner",
 	correo: "admin@ezread.ml",
-	password: "688787d8ff144c502c7f5cffaafe2cc588d86079f9de88304c26b0cb99ce91c6",
+	password: "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4",
 	telefono: "22334455",
 	direccion: "",
 	activo: 1,
