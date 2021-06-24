8998d5b4c6c8a909c8e273bd444dd7e8f7bcffbc
Guillermo Peitzner <guillermopeitzner@gmail.com>
2021-06-24 01:42:19 -0600

    [fix] run script tests

--- e2e/run.bat
+++ e2e/run.bat
@@ -1,18 +1,18 @@
 ECHO "Executing e2e tests ..."
-ECHO "Runing test 1 ..."
+ECHO "Running test 1 ..."
 python ./test1.py
-ECHO "Runing test 2 ..."
+ECHO "Running test 2 ..."
 python ./test2.py
-ECHO "Runing test 3 ..."
+ECHO "Running test 3 ..."
 python ./test3.py
-ECHO "Runing test 4 ..."
+ECHO "Running test 4 ..."
 python ./test4.py
-ECHO "Runing test 5 ..."
+ECHO "Running test 5 ..."
 python ./test5.py
-ECHO "Runing test 6 ..."
+ECHO "Running test 6 ..."
 python ./test6.py
-ECHO "Runing test 7 ..."
+ECHO "Running test 7 ..."
 python ./test7.py
-ECHO "Runing test 8 ..."
+ECHO "Running test 8 ..."
 python ./test8.py
 ECHO "Everything it' ok :)"
--- e2e/run.sh
+++ e2e/run.sh
@@ -1,18 +1,18 @@
 echo "Executing e2e tests ..."
-echo "Runing test 1 ..."
+echo "Running test 1 ..."
 python ./test1.py
-echo "Runing test 2 ..."
+echo "Running test 2 ..."
 python ./test2.py
-echo "Runing test 3 ..."
+echo "Running test 3 ..."
 python ./test3.py
-echo "Runing test 4 ..."
+echo "Running test 4 ..."
 python ./test4.py
-echo "Runing test 5 ..."
+echo "Running test 5 ..."
 python ./test5.py
-echo "Runing test 6 ..."
+echo "Running test 6 ..."
 python ./test6.py
-echo "Runing test 7 ..."
+echo "Running test 7 ..."
 python ./test7.py
-echo "Runing test 8 ..."
+echo "Running test 8 ..."
 python ./test8.py
 echo "Everything it' ok :)"
