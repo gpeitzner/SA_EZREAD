ceaf00127f6ea0938fba98cc880410d5ee7399df
Guillermo Peitzner <guillermopeitzner@gmail.com>
2021-06-24 00:51:55 -0600

    [add] publisher creation account failure test

--- e2e/test5.py
+++ e2e/test5.py
@@ -7,13 +7,28 @@ import time
 class test5(unittest.TestCase):
 
     def setUp(self):
+        HUB_URL = "localhost"
+        HOST_URL = "192.168.1.100"
         self.driver = webdriver.Remote(
-            command_executor="http://localhost:4444/wd/hub", desired_capabilities=DesiredCapabilities.FIREFOX)
-        self.driver.get("http://192.168.1.100:8080/login")
+            command_executor="http://"+HUB_URL+":4444/wd/hub", desired_capabilities=DesiredCapabilities.FIREFOX)
+        self.driver.get("http://"+HOST_URL+":8080/register")
         time.sleep(5)
 
-    def test_correct_authentication(self):
-        pass
+    def test5(self):
+        publisher_area = self.driver.find_element_by_xpath(
+            "//input[@value='publisher']")
+        publisher_area.click()
+        name_input = self.driver.find_element_by_id("nombre")
+        name_input.send_keys("Editorial Vita")
+        address_input = self.driver.find_element_by_id("direccion")
+        address_input.send_keys(
+            "15 Calle B 23-32 Zona 10 de Guatemala, Guatemala")
+        email_input = self.driver.find_element_by_id("correo")
+        email_input.send_keys("editorialvita@gmail.com")
+        signup_button = self.driver.find_element_by_class_name(
+            "MuiButton-label")
+        signup_button.click()
+        time.sleep(5)
 
     def tearDown(self):
         self.driver.quit()
