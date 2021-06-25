eabbf713d333bb282bb94313a2704d41c8b28504
Guillermo Peitzner <guillermopeitzner@gmail.com>
2021-06-24 21:39:21 -0600

    [fix] python bin

--- e2e/run.sh
+++ e2e/run.sh
@@ -1,18 +1,18 @@
 echo "Executing e2e tests ..."
 echo "Running test 1 ..."
-python ./test1.py
+python3 ./test1.py
 echo "Running test 2 ..."
-python ./test2.py
+python3 ./test2.py
 echo "Running test 3 ..."
-python ./test3.py
+python3 ./test3.py
 echo "Running test 4 ..."
-python ./test4.py
+python3 ./test4.py
 echo "Running test 5 ..."
-python ./test5.py
+python3 ./test5.py
 echo "Running test 6 ..."
-python ./test6.py
+python3 ./test6.py
 echo "Running test 7 ..."
-python ./test7.py
+python3 ./test7.py
 echo "Running test 8 ..."
-python ./test8.py
+python3 ./test8.py
 echo "Everything it' ok :)"
