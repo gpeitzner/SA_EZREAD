d573ee4f34f183009b70d7370865a9ed6889d0ce
Guillermo Peitzner <guillermopeitzner@gmail.com>
2021-06-26 16:11:04 -0600

    [add] user proxy

--- src/backend/esb/routes/users.js
+++ src/backend/esb/routes/users.js
@@ -1,9 +1,34 @@
 var express = require("express");
 var router = express.Router();
+const axios = require("axios").default;
+const host = process.env.HOST_URL || "localhost";
 
 /**GET users listing. */
 router.get("/", function (req, res, next) {
 	res.send("users");
+});
+
+/**Signup users */
+router.post("/signup", function (req, res, next) {
+	axios
+		.post(`http://${host}:5002/users`, req.body)
+		.then((response) => {
+			res.json(response.data);
+		})
+		.catch((error) => {
+			res.json(error);
+		});
+});
+
+router.post("/login", function (req, res, next) {
+	axios
+		.post(`http://${host}:5000/`, req.body)
+		.then((response) => {
+			res.json(response.data);
+		})
+		.catch((error) => {
+			res.json(error);
+		});
 });
 
 module.exports = router;
